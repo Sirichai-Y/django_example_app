b6d9417c9107b7b036ed48c43ea9730ac6e09169
Sirichai-Y <sirichai.ykt@mail.kmutt.ac.th>
2020-05-05 21:53:33 +0700

    add comment to view & url

--- backend/urls.py
+++ backend/urls.py
@@ -4,8 +4,15 @@ from backend import views
 
 urlpatterns = [
     path('province/', views.province),
+
     path('situation/', views.situation),
+
+    path('comment/', views.comment),
+    path('comment/manage/<int:cid>', views.manage_comment),
+
     path('message/', views.message),
+    path('message/heart/<int:mid>', views.give_heart_message),
+    path('message/manage/<int:mid>', views.manage_message),
     path('message/prov/<int:prov>', views.message_by_province),
     path('message/sit/<int:sit>', views.message_by_situation),
     path('message/sit/<int:sit>/prov/<int:prov>', views.message_by_situation_province)
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,9 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Province, Message, Situation
-from .serializers import ProvinceSerializer, SituationSerializer, MessageSerializer, CreateMessageSerializer
+from .models import Province, Message, Situation, Comment
+from .serializers import ProvinceSerializer, SituationSerializer, MessageSerializer
+from .serializers import CreateMessageSerializer, CommentSerializer, CreateCommentSerializer
 
 @api_view(['GET'])
 def province(request):
@@ -29,6 +30,37 @@ def message(request):
             return  Response(message_data.data, status=status.HTTP_201_CREATED)
         return Response(message_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['PUT'])
+def give_heart_message(request, mid):
+    try:
+        message = Message.objects.get(id=mid)
+        heart = MessageSerializer(message).data
+        heart["message_like"] += 1
+    except Message.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    message_data = MessageSerializer(message, data=heart)
+    if message_data.is_valid():
+        message_data.save()
+        return  Response(message_data.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def manage_message(request, mid):
+    try:
+        message = Message.objects.get(id=mid)
+    except Message.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        message_data = MessageSerializer(message, data=request.data)
+        if message_data.is_valid():
+            message_data.save()
+            return  Response(message_data.data)
+    if request.method == 'DELETE':
+        message.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 @api_view(['GET'])
 def message_by_province(request, prov):
     message = Message.objects.filter(message_to=prov)
@@ -46,3 +78,28 @@ def message_by_situation_province(request, sit, prov):
     message = Message.objects.filter(message_situation=sit, message_to=prov)
     serializer = MessageSerializer(message, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def comment(request):
+    comment_data = CreateCommentSerializer(data=request.data)
+    if comment_data.is_valid():
+        comment_data.save()
+        return  Response(comment_data.data, status=status.HTTP_201_CREATED)
+    return Response(comment_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def manage_comment(request, cid):
+    try:
+        comment = Comment.objects.get(id=cid)
+    except Comment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        comment_data = CommentSerializer(comment, data=request.data)
+        if comment_data.is_valid():
+            comment_data.save()
+            return  Response(comment_data.data, status=status.HTTP_201_CREATED)
+        return Response(comment_data.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
